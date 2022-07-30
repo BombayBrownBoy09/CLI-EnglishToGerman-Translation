989b039bc110e216c2270dcd79715f226e20a24a
Bhargav Shetgaonkar <67541365+BombayBrownBoy09@users.noreply.github.com>
2022-07-30 15:15:32 -0400

    Update test_translator.py

--- test_translator.py
+++ test_translator.py
@@ -12,13 +12,21 @@ def test_get_article_text():
     assert len(article_txt) > 100
 
 
-def test_translation():
-    runner = CliRunner()
-    result = runner.invoke(
-        get_and_translate,
-        ["https://en.wikipedia.org/wiki/Monarchy_of_Germany"],
+# def test_translation():
+#     runner = CliRunner()
+#     result = runner.invoke(
+#         get_and_translate,
+#     )
+#     assert result.output.__contains__(
+#         "Monarchie Deutschlands"
+#     )
+#     assert len(result.output) > 20
+
+def get_and_translate():
+    article_txt = get_article_text(
+        "https://en.wikipedia.org/wiki/Monarchy_of_Germany"
     )
-    assert result.output.__contains__(
-        "Monarchie Deutschlands"
+    translation = translate(article_txt)
+    assert translation.__contains__(
+        "Titel Deutscher Kaiser"
     )
-    assert len(result.output) > 20
