68ba106e3c9577335550820c9da9d976760f0c1e
Bhargav Shetgaonkar <67541365+BombayBrownBoy09@users.noreply.github.com>
2022-07-30 14:35:33 -0400

    Update translator.py

--- translator.py
+++ translator.py
@@ -6,8 +6,7 @@ from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 def translate(input_text, min_length, max_length, model, tokenizer):
     inputs = tokenizer(
-    "translate English to German: Hugging Face is a technology company based in New York and Paris",
-    return_tensors="pt")
+    input_text, return_tensors="pt", max_length=1024, truncation=True)
     outputs = model.generate(inputs["input_ids"], max_length=max_length, num_beams=4, early_stopping=True)
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
